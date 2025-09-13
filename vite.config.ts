ac645e6492c450e17db6a0858831a5e11a363007
Sofiane Khenchelaoui <sofianekhenchelaoui1@gmail.com>
2025-09-13 13:41:02 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/skhdev/", // <--- Ajoutez cette ligne
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
